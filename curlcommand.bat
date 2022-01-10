4c650d6c5a4872afa9140275fe72d2d97e0343c8
Garret Martinez <93949389+GarretMartinez@users.noreply.github.com>
2022-01-10 13:26:33 -0700

    Update curlcommand.bat

--- curlcommand.bat
+++ curlcommand.bat
@@ -1,1 +1,1 @@
-curl -d '{"userName":"john@john.com", "password":"P@55word"}' -H "Content-Type: application/json" -X POST https://dev.stedi.me/login
+curl -d "@data.json" -X POST "https://dev.stedi.me/login"
