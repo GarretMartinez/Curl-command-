798ffc69cc82ff872170f4bad27da5fe7b573d5b
GarretMartinez <gtmrz22@gmail.com>
2022-01-26 13:36:27 -0700

    Updated 1-26-22

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const port = 3000;
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
     res.send("Hello browser");
 });
 
-app.listen(port, () =>{});
+app.listen(port, () =>{}); 
+
+app.post('/login', (req, res) =>{
+    if(req.body.userName == "gmoney"){
+        res.send("Welcome!")
+    } else {
+        res.send("Who are you?");
+    }
+});
