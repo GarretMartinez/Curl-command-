d1038b606437162f1d95ee11d58b6eef67bc9e24
GarretMartinez <gtmrz22@gmail.com>
2022-02-04 22:35:05 -0700

    2/4/22

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const md5 = require('md5');
-
 const port = 3000;
-
 const app = express();
+const https = require('https')
+const fs = require('fs')
+
+
 
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.send("Hello browser");
+    res.send("Hello HTTPS!");
 });
 
-app.listen(port, () =>{}); 
+https.createServer({
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.cert')
+  }, app).listen(3000, () => {
+    console.log('Listening...')
+  })
+
+//app.listen(port, () =>{}); 
 
 app.post('/login', (req, res) =>{
     if(req.body.userName == "gmoney" && md5(req.body.password) == "fc4d477402d75c8cf7c37e07d68ada9c"){
