37477b185061965c045e539f2390938634417f06
GarretMartinez <gtmrz22@gmail.com>
2022-03-17 17:13:41 -0600

    3-17

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 4443;
 const app = express();
 const https = require('https')
 const fs = require('fs')
-
+let invalidLoginAttemps = 0
 app.use(express.static('public'));
 
 app.use(bodyParser.json());
@@ -24,9 +24,17 @@ https.createServer({
 //app.listen(port, () =>{}); 
 
 app.post('/login', (req, res) =>{
-    if(req.body.userName == "gmoney" && md5(req.body.password) == "fc4d477402d75c8cf7c37e07d68ada9c"){
+    console.log(JSON.stringify(req,body));
+    if(invalidLoginAttemps>=5){
+        res.status(401);
+        res.send("Max attemtps reached")
+    }
+    else if(req.body.userName == "gmoney" && md5(req.body.password) == "fc4d477402d75c8cf7c37e07d68ada9c"){
         res.send("Welcome!")
     } else {
+        invalidLoginAttemps ++;///maybe delete
+        //console.log(invalidLoginAttempts+" invalid attempts");
+        res.status(401);//unauthorized
         res.send("Who are you?");
     }
 });
